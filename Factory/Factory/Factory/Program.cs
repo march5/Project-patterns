196add32efde4f97c6c6b2b07507938e1872341a
March5 <marchewkad09@gmail.com>
2021-10-17 17:32:08 +0200

    task3 first commit

---
+++ Factory/Factory/Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factory
+{
+
+    public abstract class Tax
+    {
+
+    }
+
+    public abstract class SupplementaryPayment
+    {
+        public abstract void calculate();
+    }
+
+    public abstract class Factory
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseAmount = 100000;
+
+            /*Factory fact = Factory.get();
+            SupplementPayment sp = fact.createSP();
+            Double amountToTax = sp.calculate(baseAmount);
+            Tax tax = fact.createTax();
+            tax.calculate(amountToTax);*/
+        }
+    }
+}
