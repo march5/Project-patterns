4f3271f056cca4db00663ea48a0e4920b4ba16f8
Dominik Marchewka <marchewkad09@gmail.com>
2021-12-09 17:09:22 +0100

    Chain and Observer done

---
+++ ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+
+        public interface IHandler
+        {
+            IHandler SetNext(IHandler handler);
+
+            object Handle(object request);
+        }
+
+        abstract class AbstractHandler : IHandler
+        {
+            private IHandler nextHandler;
+
+            public IHandler SetNext(IHandler handler)
+            {
+                this.nextHandler = handler;
+                return handler;
+            }
+
+            public virtual object Handle(object request)
+            {
+                if (this.nextHandler != null)
+                    return this.nextHandler.Handle(request);
+                else return null;
+            }
+        }
+
+        class UnderPanel : AbstractHandler
+        {
+            public override object Handle(object request)
+            {
+                if((request as string) == "pic")
+                {
+                    Console.WriteLine("UnderPanel displays a picture clicked");
+                }
+
+                return base.Handle(request);
+            }
+        }
+
+        class Panel : AbstractHandler
+        {
+            public override object Handle(object request)
+            {
+                if ((request as string) == "text")
+                    Console.WriteLine("Panel displays text clicked");
+                return base.Handle(request);
+            }
+        }
+
+        class Window : AbstractHandler
+        {
+            public override object Handle(object request)
+            {
+                Console.WriteLine("Window displays button after click");
+                return base.Handle(request);
+            }
+        }
+
+        class OnClick
+        {
+            public static void Click(AbstractHandler handler)
+            {
+                foreach(var click in new List<string>{"pic", "text" })
+                {
+                    Console.WriteLine("Client clicked on " + click);
+
+                    var result = handler.Handle(click);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            UnderPanel underPanel = new UnderPanel();
+            Panel panel = new Panel();
+            Window window = new Window();
+
+            underPanel.SetNext(panel).SetNext(window);
+
+            OnClick.Click(underPanel);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Observer/Observer/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    class Program
+    {
+
+        public interface IObserver
+        {
+            void Update(ISubject subject);
+        }
+
+        public interface ISubject
+        {
+            void Attach(IObserver observer);
+
+            void Detach(IObserver observer);
+
+            void Notify();
+        }
+
+        public class OnClick : ISubject
+        {
+            public int State { get; set; } = -0;
+
+            private List<IObserver> observers = new List<IObserver>();
+
+            public void Attach(IObserver observer)
+            {
+                this.observers.Add(observer);
+            }
+
+            public void Detach(IObserver observer)
+            {
+                this.observers.Remove(observer);
+            }
+
+            public void Notify()
+            {
+                Console.WriteLine("OnClick notifying observers on client click");
+
+                foreach (var observer in observers)
+                {
+                    observer.Update(this);
+                }
+            }
+
+            public void Click(string target)
+            {
+                Console.WriteLine("Client just clicked on " + target);
+                if (target == "pic") this.State = 1;
+                else this.State = 2;
+
+                Console.WriteLine("OnClick state just changed to: " + this.State);
+                this.Notify();
+            }
+        }
+        public class UnderPanel : IObserver
+        {
+            public void Update(ISubject subject)
+            {
+                if ((subject as OnClick).State == 1)
+                {
+                    Console.WriteLine("UnderPanel displays a picture clicked");
+                }
+            }
+        }
+
+        public class Panel : IObserver
+        {
+            public void Update(ISubject subject)
+            {
+                if ((subject as OnClick).State == 2)
+                {
+                    Console.WriteLine("Panel displays text clicked");
+                }
+            }
+        }
+
+        public class Window : IObserver
+        {
+            public void Update(ISubject subject)
+            {
+                Console.WriteLine("Window displays button after click");
+            }
+        }
+        static void Main(string[] args)
+        {
+            OnClick onClick = new OnClick();
+            UnderPanel underPanel = new UnderPanel();
+            onClick.Attach(underPanel);
+
+            Panel panel = new Panel();
+            onClick.Attach(panel);
+
+            Window window = new Window();
+            onClick.Attach(window);
+
+            onClick.Click("pic");
+            Console.WriteLine();
+
+            onClick.Click("text");
+            Console.WriteLine();
+        }
+    }
+}
